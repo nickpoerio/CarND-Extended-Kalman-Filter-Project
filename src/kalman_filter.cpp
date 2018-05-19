168870f44351ffb4b55a0941596bac3fe2b906c1
nickpoerio <37049226+nickpoerio@users.noreply.github.com>
2018-05-20 01:06:11 +0200

    mod 9#

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -49,17 +49,17 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   z_pred << 0,0,0;
   
   z_pred(0) = sqrt(x_(0)*x_(0)+x_(1)*x_(1));
-  // avoid divide by zero
+  // avoid divide by zero, skipping the update
   if (z_pred(0)<1e-4) 
-    z_pred(0)=1e-4;
+    return;
 	
   z_pred(1) = atan2(x_(1),x_(0));
   // angles between -PI and PI
-  float pi =3.141592;
-  while (z_pred(1)>pi)
-	z_pred(1)-=2*pi;
-  while (z_pred(1)<-pi)
-	z_pred(1)+=2*pi;
+
+  while (z_pred(1)>M_PI)
+	z_pred(1)-=2*M_PI;
+  while (z_pred(1)<-M_PI)
+	z_pred(1)+=2*M_PI;
 	
   z_pred(2) = (x_(0)*x_(2)+x_(1)*x_(3))/z_pred(0);
   
