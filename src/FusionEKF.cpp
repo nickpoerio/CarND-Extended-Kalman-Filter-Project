58bbce10366e2ac94a2d35d45309c966f720dadf
nickpoerio <37049226+nickpoerio@users.noreply.github.com>
2018-05-20 01:54:52 +0200

    mod #11

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -161,6 +161,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
+	Tools tools;
     Hj_ = tools.CalculateJacobian(ekf_.x_);
     ekf_.H_ = Hj_;
     ekf_.R_ = R_radar_;
